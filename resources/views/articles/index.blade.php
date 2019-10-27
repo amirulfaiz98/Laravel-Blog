211fde8c133a18ed08727988bedd56bbbba0c8f3
Amirul Faiz <amirulfaiz.work@gmail.com>
2019-10-27 14:29:09 +0800

    Add edit & delete button

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -370,7 +370,10 @@
                                             <td>{{ $article->id}}</td>
                                             <td>{{ $article->title}}</td>
                                             <td>{{ $article->created_at}}</td>
-                                            <td></td>
+                                            <td>
+                                                <a href="" class="btn btn-primary">Edit</a>
+                                                <a href="" class="btn btn-danger">Delete</a>
+                                            </td>
                                         </tr>
                                     @endforeach
                                 </tbody>
